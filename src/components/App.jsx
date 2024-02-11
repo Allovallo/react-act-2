d16011d039b2e7ae01418671127091f4fb7ec2c9
Allovallo <30566134+Allovallo@users.noreply.github.com>
2024-02-11 20:34:36 +0200

    files updated

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,12 @@
 import { PageTitle } from "./PageTitle/PageTitlle"
+import { EventBoard } from "./EventBoard/EventBoard"
+import upcomingEvents from '../upcoming-events.json'
 
 export const App = () => {
-  return <div><PageTitle text="24th Core Worlds Coalition Conference" /></div>
+  return (
+    <div>
+      <PageTitle text="24th Core Worlds Coalition Conference" />
+      <EventBoard events={upcomingEvents} />
+    </div>
+  );
 }
