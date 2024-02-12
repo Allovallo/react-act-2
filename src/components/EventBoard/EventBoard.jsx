988a23da8606ebf3440d0539e674a63f0646ee1e
Allovallo <30566134+Allovallo@users.noreply.github.com>
2024-02-12 20:10:15 +0200

    files updated

--- src/components/EventBoard/EventBoard.jsx
+++ src/components/EventBoard/EventBoard.jsx
@@ -1,7 +1,7 @@
 import css from './EventBoard.module.css'
 
 export const EventBoard = ({ events }) => {
-    console.log(events);
+    // console.log(events);
     return (
         <div className={css.EventBoard}>
             {events.map(event => (
