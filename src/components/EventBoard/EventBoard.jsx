ce0352545e40277f62092f8f635e6d0bd4c9f0c3
Allovallo <30566134+Allovallo@users.noreply.github.com>
2024-02-13 20:46:52 +0200

    Event files added

--- src/components/EventBoard/EventBoard.jsx
+++ src/components/EventBoard/EventBoard.jsx
@@ -3,7 +3,7 @@ import css from './EventBoard.module.css'
 export const EventBoard = ({ events }) => {
     // console.log(events);
     return (
-        <div className={css.EventBoard}>
+        <div className={css.eventBoard}>
             {events.map(event => (
                 <div key={event.name}>{event.name}</div>
             ))}
