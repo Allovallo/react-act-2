b3834e5cd29b0534fe77f71a803590b0ce885951
Allovallo <30566134+Allovallo@users.noreply.github.com>
2024-02-15 17:55:25 +0200

    files updated

--- src/components/Event/Event.jsx
+++ src/components/Event/Event.jsx
@@ -1,27 +1,30 @@
 import PropTypes from 'prop-types';
+import css from './Event.module.css'
 
 export const Event = ({ name, location, speaker, type, start, end }) => {
-    return (<div className="event">
-        <h2 className="title"></h2>
-        <p className="info">
-            <i className="icon"></i>
-            Location
-        </p>
-        <p className="info">
-            <i className="icon"></i>
-            Speaker
-        </p>
-        <p className="info">
-            <i className="icon"></i>
-            Start Date
-        </p>
-        <p className="info">
-            <i className="icon"></i>
-            Duration
-        </p>
-        {/* <span className="chip free|paid|vip">Event type</span> */}
-    </div>)
-}
+    return (
+        <div className={css.event}>
+            <h2 className={css.title}>{name}</h2>
+            <p className={css.info}>
+                <i className="icon"></i>
+                {location}
+            </p>
+            <p className={css.info}>
+                <i className="icon"></i>
+                {speaker}
+            </p>
+            <p className={css.info}>
+                <i className="icon"></i>
+                {start}
+            </p>
+            <p className={css.info}>
+                <i className="icon"></i>
+                Duration
+            </p>
+            {/* <span className="chip free|paid|vip">Event type</span> */}
+        </div>
+    );
+};
 
 Event.propTypes = {
     name: PropTypes.string.isRequired,
