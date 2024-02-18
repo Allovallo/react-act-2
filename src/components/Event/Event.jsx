e73528207f1dce90a596ee7a2e2335a10b5874b0
Allovallo <30566134+Allovallo@users.noreply.github.com>
2024-02-18 18:54:09 +0200

    files updated

--- src/components/Event/Event.jsx
+++ src/components/Event/Event.jsx
@@ -1,8 +1,6 @@
 import PropTypes from 'prop-types';
 import { FaMapMarkerAlt, FaUserAlt, FaCalendarAlt, FaClock } from "react-icons/fa";
-
-// import { formatEventStart } from 'utils/formatEventStart';
-// import { formatEventDuration } from 'utils/formatEventDuration';
+import { iconSize } from 'components/constants';
 import { formatEventStart, formatEventDuration } from 'utils';
 
 import css from './Event.module.css'
@@ -10,26 +8,30 @@ import css from './Event.module.css'
 export const Event = ({ name, location, speaker, type, start, end }) => {
     const formattedStart = formatEventStart(start);
     const duration = formatEventDuration(start, end)
+
+    console.log(css);
+    console.log(css[type]);
+
     return (
         <div className={css.event}>
             <h2 className={css.title}>{name}</h2>
             <p className={css.info}>
-                <FaMapMarkerAlt className={css.icon} size={16} />
+                <FaMapMarkerAlt className={css.icon} size={iconSize.md} />
                 {location}
             </p>
             <p className={css.info}>
-                <FaUserAlt className={css.icon} size={16} />
+                <FaUserAlt className={css.icon} size={iconSize.md} />
                 {speaker}
             </p>
             <p className={css.info}>
-                <FaCalendarAlt className={css.icon} size={16} />
+                <FaCalendarAlt className={css.icon} size={iconSize.md} />
                 {formattedStart}
             </p>
             <p className={css.info}>
-                <FaClock className={css.icon} size={16} />
+                <FaClock className={css.icon} size={iconSize.md} />
                 {duration}
             </p>
-            {/* <span className="chip free|paid|vip">Event type</span> */}
+            <span className={`${css.chip} ${css[type]}`}>{type}</span>
         </div>
     );
 };
